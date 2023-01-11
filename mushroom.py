4105f3af35d1c254bed75b67df9486a7fe595c8b
20F20546-mec <118101059+20F20546-mec@users.noreply.github.com>
2023-01-12 00:01:09 +0400

    Updated and added files
    Updated python files with comments
    Added task 2 pdf for output of python files

--- mushroom.py
+++ mushroom.py
@@ -1,17 +1,25 @@
+# Import the necessary libraries
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
 
+# Used to convert the strings to numeric value
 train_data = pd.read_csv('mushrooms.csv')
 
+# The head (first 5 elements) is printed
 print(train_data.head())
 
+# Read data from the dataset
 pr = preprocessing.LabelEncoder()
 
+# All the dataset is transformed into numeric values
+
+# This data is the target variable
 train_data['class'] = pr.fit_transform(train_data['class'])
 
+# The rest of the data is the feature variable
 train_data['cap-shape'] = pr.fit_transform(train_data['cap-shape'])
 train_data['cap-surface'] = pr.fit_transform(train_data['cap-surface'])
 train_data['cap-color'] = pr.fit_transform(train_data['cap-color'])
@@ -35,20 +43,28 @@ train_data['spore-print-color'] = pr.fit_transform(train_data['spore-print-color
 train_data['population'] = pr.fit_transform(train_data['population'])
 train_data['habitat'] = pr.fit_transform(train_data['habitat'])
 
+# The X_train is for training with the features
 X_train = train_data[['cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment', 'gill-spacing',
                       'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring',
                       'stalk-surface-below-ring', 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type',
                       'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat']]
+
+# The Y_train is for training with the target variable
 Y_train = train_data[['class']]
 
+# The train_test_split method is used to split the data into training and testing data
 X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.3, random_state=0)
 
+# The MLPClassifier function is used to classify the dataset
 classifier = MLPClassifier(hidden_layer_sizes=(6, 5), random_state=0, verbose=True, learning_rate_init=0.01)
 
+# The model is built
 classifier.fit(X_train, Y_train)
 
+# The prediction happens here
 Y_pred = classifier.predict(X_test)
 
+# The performance metrics are printed here
 print('\n Accuracy: {}%'.format(round(metrics.accuracy_score(Y_test, Y_pred), 2)*100))
 print('\n Precision: {}%'.format(round(metrics.precision_score(Y_test, Y_pred,), 2)*100))
 print('\n Recall: {}%'.format(round(metrics.recall_score(Y_test, Y_pred), 2)*100))
--- tanh.py
+++ tanh.py
@@ -1,3 +1,4 @@
+# Import the necessary libraries
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
@@ -6,12 +7,18 @@ from keras.layers import Dense, Activation
 from keras.optimizers import Adam
 from keras.metrics import sparse_categorical_crossentropy
 
+# Used to convert the strings to numeric value
 pr = preprocessing.LabelEncoder()
 
+# Read data from the dataset
 train_data = pd.read_csv('mushrooms.csv')
 
+# All the dataset is transformed into numeric values
+
+# This data is the target variable
 train_data['class'] = pr.fit_transform(train_data['class'])
 
+# The rest of the data is the feature variable
 train_data['cap-shape'] = pr.fit_transform(train_data['cap-shape'])
 train_data['cap-surface'] = pr.fit_transform(train_data['cap-surface'])
 train_data['cap-color'] = pr.fit_transform(train_data['cap-color'])
@@ -35,29 +42,35 @@ train_data['spore-print-color'] = pr.fit_transform(train_data['spore-print-color
 train_data['population'] = pr.fit_transform(train_data['population'])
 train_data['habitat'] = pr.fit_transform(train_data['habitat'])
 
+# The X_train is for training with the features
 X_train = train_data[['cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment', 'gill-spacing',
                       'gill-size', 'gill-color', 'stalk-shape', 'stalk-root', 'stalk-surface-above-ring',
                       'stalk-surface-below-ring', 'stalk-color-above-ring', 'stalk-color-below-ring', 'veil-type',
                       'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population', 'habitat']]
+
+# The Y_train is for training with the target variable
 Y_train = train_data[['class']]
 
+# The train_test_split method is used to split the data into training and testing data
 X_train, X_test, Y_train, Y_test = train_test_split(X_train, Y_train, test_size=0.3, random_state=0)
 
+# The number of nodes and activation function is decided here
 layers = [
-    Dense(units=16, input_shape=(22,), activation='relu'),
+    Dense(units=20, input_shape=(22,), activation='relu'),
     Dense(units=2, activation='tanh')
 ]
 
+# Building the model
 model = Sequential(layers)
 
+# The model is compiled
 model.compile(
     optimizer=Adam(learning_rate=0.0001),
     loss=sparse_categorical_crossentropy,
     metrics=['accuracy']
 )
 
-model.evaluate(X_train, Y_train)
-
+# Training the model
 model.fit(
     x=X_train,
     y=Y_train,
@@ -66,3 +79,10 @@ model.fit(
     verbose=2,
     shuffle=True
 )
+
+# Evaluating the loss and accuracy of the model
+test_loss, test_acc = model.evaluate(X_train, Y_train)
+
+# The loss and accuracy is printed
+print("Test loss with tanh activation:", str((test_loss*100)) + "%")
+print("Test accuracy with tanh activation:", str((test_acc*100)) + "%")
